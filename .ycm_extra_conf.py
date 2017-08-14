14e3318958f6845ef70531e2093ea76d7e21be29
李宗沅 <952969055@qq.com>
2017-08-14 15:28:19 +0800

    重写了ycm的默认配置文件，详情请参看README的ycm插件说明部分

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -14,8 +14,6 @@ BASE_FLAGS = [
         '-fexceptions',
         '-ferror-limit=10000',
         '-DNDEBUG',
-        '-std=POSIX.1-2008',
-        '-xc',
         '-isystem',
         '/usr/include/',
         '-isystem',
@@ -28,9 +26,17 @@ SOURCE_EXTENSIONS = [
         '.cpp',
         '.cxx',
         '.cc',
-        '.c',
-        '.m',
-        '.mm'
+        '.c'
+        ]
+
+C_SOURCE_EXTENSIONS = [
+        '.cc',
+        '.c'
+        ]
+
+CPP_SOURCE_EXTENSIONS = [
+        '.cpp',
+        '.cxx'
         ]
 
 SOURCE_DIRECTORIES = [
@@ -52,137 +58,75 @@ HEADER_DIRECTORIES = [
 
 BUILD_DIRECTORY = 'build';
 
-def IsHeaderFile(filename):
-    extension = os.path.splitext(filename)[1]
-    return extension in HEADER_EXTENSIONS
+def isHeaderFile(filename):
+    ext = os.path.splitext(filename)[1]
+    return ext in HEADER_EXTENSIONS
 
-def GetCompilationInfoForFile(database, filename):
-    if IsHeaderFile(filename):
-        basename = os.path.splitext(filename)[0]
-        for extension in SOURCE_EXTENSIONS:
-            # Get info from the source files by replacing the extension.
-            replacement_file = basename + extension
-            if os.path.exists(replacement_file):
-                compilation_info = database.GetCompilationInfoForFile(replacement_file)
-                if compilation_info.compiler_flags_:
-                    return compilation_info
-            # If that wasn't successful, try replacing possible header directory with possible source directories.
-            for header_dir in HEADER_DIRECTORIES:
-                for source_dir in SOURCE_DIRECTORIES:
-                    src_file = replacement_file.replace(header_dir, source_dir)
-                    if os.path.exists(src_file):
-                        compilation_info = database.GetCompilationInfoForFile(src_file)
-                        if compilation_info.compiler_flags_:
-                            return compilation_info
-        return None
-    return database.GetCompilationInfoForFile(filename)
+def isCSourceFile(filename):
+    ext = os.path.splitext(filename)[1];
+    return ext in C_SOURCE_EXTENSIONS;
 
-def FindNearest(path, target, build_folder=None):
+def isCPPSourceFile(filename):
+    ext = os.path.splitext(filename)[1];
+    return ext in CPP_SOURCE_EXTENSIONS;
+
+def findNearest(path, target):
     candidate = os.path.join(path, target)
     if(os.path.isfile(candidate) or os.path.isdir(candidate)):
-        logging.info("Found nearest " + target + " at " + candidate)
         return candidate;
 
     parent = os.path.dirname(os.path.abspath(path));
     if(parent == path):
         return None
 
-    if(build_folder):
-        candidate = os.path.join(parent, build_folder, target)
-        if(os.path.isfile(candidate) or os.path.isdir(candidate)):
-            logging.info("Found nearest " + target + " in build folder at " + candidate)
-            return candidate;
+    return findNearest(parent, target)
 
-    return FindNearest(parent, target, build_folder)
+def getModuleIncludeFlags(module_root):
+    flags = [];
 
-def MakeRelativePathsInFlagsAbsolute(flags, working_directory):
-    if not working_directory:
-        return list(flags)
-    new_flags = []
-    make_next_absolute = False
-    path_flags = [ '-isystem', '-I', '-iquote', '--sysroot=' ]
-    for flag in flags:
-        new_flag = flag
+    for dirname in os.listdir(module_root):
+        if dirname in HEADER_DIRECTORIES:
+            flags = flags + ['-I', os.path.join(module_root, dirname)];
 
-        if make_next_absolute:
-            make_next_absolute = False
-            if not flag.startswith('/'):
-                new_flag = os.path.join(working_directory, flag)
-
-        for path_flag in path_flags:
-            if flag == path_flag:
-                make_next_absolute = True
-                break
-
-            if flag.startswith(path_flag):
-                path = flag[ len(path_flag): ]
-                new_flag = path_flag + os.path.join(working_directory, path)
-                break
-
-        if new_flag:
-            new_flags.append(new_flag)
-    return new_flags
+    return flags;
 
 
-def FlagsForClangComplete(root):
-    try:
-        clang_complete_path = FindNearest(root, '.clang_complete')
-        clang_complete_flags = open(clang_complete_path, 'r').read().splitlines()
-        return clang_complete_flags
-    except:
-        return None
+def getIncludeFlags(root, filename):
+    include_flags = [];
 
-def FlagsForInclude(root):
-    try:
-        flags = []
-        for header_dir in HEADER_DIRECTORIES:
-            include_path = FindNearest(root, header_dir)
-            if include_path == None:
-                continue;
+    # find nearest dir in HEADER_DIRECTORIES
+    for dirname in HEADER_DIRECTORIES:
+        include_path = findNearest(root, dirname)
+        if include_path:
+            include_flags = include_flags + ['-I', include_path];
 
-            flags = flags + ["-I"+include_path]
-            for dirroot, dirnames, filenames in os.walk(include_path):
-                for dir_path in dirnames:
-                    real_path = os.path.join(dirroot, dir_path)
-                    flags = flags + ["-I" + real_path]
-        return flags
-    except:
-        return None
+    # collection third party include info
+    for dirname in os.listdir(root):
+        if (os.path.isdir(dirname)):
+            include_flags = include_flags + getModuleIncludeFlags(os.path.join(root, dirname))
 
-def FlagsForCompilationDatabase(root, filename):
-    try:
-        # Last argument of next function is the name of the build folder for
-        # out of source projects
-        compilation_db_path = FindNearest(root, 'compile_commands.json', BUILD_DIRECTORY)
-        compilation_db_dir = os.path.dirname(compilation_db_path)
-        logging.info("Set compilation database directory to " + compilation_db_dir)
-        compilation_db =  ycm_core.CompilationDatabase(compilation_db_dir)
-        if not compilation_db:
-            logging.info("Compilation database file found but unable to load")
-            return None
-        compilation_info = GetCompilationInfoForFile(compilation_db, filename)
-        if not compilation_info:
-            logging.info("No compilation info for " + filename + " in compilation database")
-            return None
-        return MakeRelativePathsInFlagsAbsolute(
-                compilation_info.compiler_flags_,
-                compilation_info.compiler_working_dir_)
-    except:
-        return None
+    return include_flags;
+
+def getLanguageFlags(filename):
+    if isCSourceFile(filename):
+        return ['-xc', '-std=c99']
+    elif isCPPSourceFile(filename):
+        return ['-xc++', '-std=c++11']
+    else:
+        return None;
+
 
 def FlagsForFile(filename):
-    root = os.path.realpath(filename);
-    compilation_db_flags = FlagsForCompilationDatabase(root, filename)
-    if compilation_db_flags:
-        final_flags = compilation_db_flags
-    else:
-        final_flags = BASE_FLAGS
-        clang_flags = FlagsForClangComplete(root)
-        if clang_flags:
-            final_flags = final_flags + clang_flags
-        include_flags = FlagsForInclude(root)
-        if include_flags:
-            final_flags = final_flags + include_flags
+    root = os.path.dirname(os.path.realpath(filename));
+
+    include_flags = getIncludeFlags(root, filename);
+    language_flags = getLanguageFlags(filename);
+
+    final_flags = BASE_FLAGS;
+    if language_flags:
+        final_flags = final_flags + language_flags
+    if include_flags:
+        final_flags = final_flags + include_flags
     return {
             'flags': final_flags,
             'do_cache': True
